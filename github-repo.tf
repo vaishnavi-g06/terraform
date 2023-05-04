9de0b04f9f8eba097a4ddc3bcd901392982d4d74
vaishnavi-g06 <131155303+vaishnavi-g06@users.noreply.github.com>
2023-05-04 16:02:24 +0530

    Update github-repo.tf

--- github-repo.tf
+++ github-repo.tf
@@ -10,7 +10,7 @@ terraform {
 
 
 provider "github" {
-  token="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"
+  token="github_pat_11A7IUKZY0nKJj6Wy4ntsY"
 
 }
 resource "github_repository" "example" {
