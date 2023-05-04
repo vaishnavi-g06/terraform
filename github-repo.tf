c19226cc13cc7cfb3ba279979e314df5d93fe048
vaishnavi-g06 <131155303+vaishnavi-g06@users.noreply.github.com>
2023-05-04 11:34:05 +0530

    Update github-repo.tf

--- github-repo.tf
+++ github-repo.tf
@@ -1,22 +1,21 @@
 
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
 
-# # Configure the GitHub Provider
 
-# provider "github" {
-#   token="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"
+provider "github" {
+  token="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"
 
-# }
-# resource "github_repository" "example" {
-#   name        = "example"
-#   description = "My awesome codebase"
+}
+resource "github_repository" "example" {
+  name        = "example"
+ 
 
-#   visibility = "public"
-# }
+  visibility = "public"
+}
